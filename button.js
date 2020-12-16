df6e07f12194cac3e370718aa037f12167a462a9
york <jason945119@gmail.com>
2020-12-16 16:10:41 +0800

    剩下 user part

--- button.js
+++ button.js
@@ -1,23 +1,38 @@
-// chrome.runtime.onMessage.addListener(
-//     function(request, sender, sendResponse) {
-//         console.log(sender.tab ?
-//             "from a content script:" + sender.tab.url :
-//             "from the extension");
-//         if (request.greeting == "hello")
-//             sendResponse({farewell: "goodbye"});
-//     }
-// );
 (function() {
     console.log('b1');
+    function SuccessMethod(e) {
+        if (e['status'] === false) {
+            alert(e.msg);
+        }
+    }
+
+    function FailMethod(e) {
+        alert(e);
+        console.log(e);
+    }
+
+    function dibbling() {
+        let promise_post_list = $.ajax({
+            url: 'https://local.dibbling.tw/api/v2/list/extension',
+            method: "POST",
+            dataType: "json",
+            data: {
+                'videoId': window.location.href,
+            },
+        });
+        promise_post_list.done(SuccessMethod);
+        promise_post_list.fail(FailMethod);
+    }
+
     let dibbling_api_token;
-    // 點播按鈕
     setTimeout(function () {
         let dibbling_button = document.createElement('paper-button');
         dibbling_button.className = 'ytd-subscribe-button-renderer';
         dibbling_button.id = 'dibbling_extension_button';
         dibbling_button.textContent = '核心點播';
         document.querySelector('#meta-contents #container #top-row').append(dibbling_button);
-    }, 2000); // check again in a second
+        document.getElementById('dibbling_extension_button').addEventListener("click", dibbling);
+    }, 2000);
 
     console.log('b2');
 })();
