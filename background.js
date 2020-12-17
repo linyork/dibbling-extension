d7c51e5ac02d957b1b3046cd8e8dc67eb8587fd0
york <jason945119@gmail.com>
2020-12-17 13:29:21 +0800

    搞定

--- background.js
+++ background.js
@@ -1,11 +1,29 @@
 function getCookies(domain, name, callback) {
     chrome.cookies.get({"url": domain, "name": name}, function(cookie) {
         if(callback) {
-            callback(cookie.value);
+            try {
+                callback(cookie.value);
+            } catch (error) {
+                callback('NO');
+            }
         }
     });
 }
-
-getCookies("https://local.dibbling.tw", "dibbling_session", function(dibbling_token) {
-    console.log(dibbling_token);
-});
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse){
+        if(request.msg == "getDibblingToken"){
+            getCookies("https://local.dibbling.tw", "dibbling_token", function(dibbling_token) {
+                if(dibbling_token === 'NO') {
+                    alert('請先登入核心點播系統');
+                    window.open('https://local.dibbling.tw/login');
+                } else {
+                    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+                        chrome.tabs.sendMessage(tabs[0].id, { greeting:'sendToken', token: dibbling_token }, function(response) {
+                            // console.log(response.farewell);
+                        });
+                    });
+                }
+            });
+        }
+    }
+);
--- button.js
+++ button.js
@@ -1,31 +1,34 @@
 (function() {
     function SuccessMethod(e) {
-        if (e['status'] === false) {
-            alert(e.msg);
-        }
-        console.log(e);
+        alert(e.msg );
     }
 
     function FailMethod(e) {
-        alert(e);
         console.log(e);
     }
 
     function dibbling() {
-        let promise_post_list = $.ajax({
-            url: 'https://local.dibbling.tw/api/v2/list/extension',
-            headers: {
-                'Authorization': 'Bearer {token}',
-            },
-            method: "POST",
-            dataType: "json",
-            data: {
-                'videoId': window.location.href,
-            },
-        });
-        promise_post_list.done(SuccessMethod);
-        promise_post_list.fail(FailMethod);
+        chrome.runtime.sendMessage({ msg: "getDibblingToken" });
     }
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        // console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension');
+        if (request.greeting ==='sendToken') {
+            let promise_post_list = $.ajax({
+                url: 'https://local.dibbling.tw/api/v2/list/extension',
+                headers: {
+                    'Authorization': 'Bearer ' + request.token,
+                },
+                method: "POST",
+                dataType: "json",
+                data: {
+                    'videoId': window.location.href,
+                },
+            });
+            promise_post_list.done(SuccessMethod);
+            promise_post_list.fail(FailMethod);
+        }
+    });
 
     setTimeout(function () {
         let dibbling_button = document.createElement('paper-button');
@@ -35,4 +38,5 @@
         document.querySelector('#meta-contents #container #top-row').append(dibbling_button);
         document.getElementById('dibbling_extension_button').addEventListener("click", dibbling);
     }, 2000);
+
 })();
