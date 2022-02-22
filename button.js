f3f7d437fda92aa360b284a850da224f377f7769
york <jason945119@gmail.com>
2022-02-22 17:44:32 +0800

    fix

--- button.js
+++ button.js
@@ -31,7 +31,7 @@
     });
 
     setTimeout(function () {
-        let dibbling_button = document.createElement('paper-button');
+        let dibbling_button = document.createElement('button');
         dibbling_button.className = 'ytd-subscribe-button-renderer';
         dibbling_button.id = 'dibbling_extension_button';
         dibbling_button.textContent = '核心點播';
